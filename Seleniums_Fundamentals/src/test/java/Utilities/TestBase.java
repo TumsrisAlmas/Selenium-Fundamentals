3ee1c70c474abf7811a5ef76b43d8e98b84081f6
TumsrisAlmas <78190444+TumsrisAlmas@users.noreply.github.com>
2021-08-10 13:58:30 -0400

    Add files via upload

---
+++ Seleniums_Fundamentals/src/test/java/Utilities/TestBase.java
@@ -0,0 +1,46 @@
+package Utilities;
+
+import PagesElements.FaceBookElements;
+import PagesElements.GoogleElements;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    public static WebDriver driver;
+
+
+    @BeforeMethod()   // controls how the @Test method will be executed.
+    public void setup01(){
+        // this before method is to setup browsers.
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.navigate().to(Mylibraray.getPropertiesData("GGWebSite"));
+
+    }
+
+    @BeforeMethod
+    public void setup02(){
+        // this before method is to run each weblemnt page' constructor. (PageFactory).
+        new FaceBookElements();
+        new GoogleElements();
+
+    }
+
+    @AfterMethod
+    public void end(){
+        driver.quit();
+        driver = null;
+    }
+
+
+
+
+
+}
